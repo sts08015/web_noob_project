b26432d1c1b023a24ca149c1198022e8669e08f7
sts08015 <sts08015@gmail.com>
2022-03-09 08:50:52 +0900

    separate js & code beautify

---
+++ js/tawk.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/622413131ffac05b1d7d271f/1fteel1mh';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
